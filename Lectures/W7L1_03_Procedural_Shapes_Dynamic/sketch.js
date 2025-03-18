2e983ce3211cec48370a3ae726a6705add1d466b
dl960 <114921992+dl960@users.noreply.github.com>
2025-03-18 19:31:44 +0000

    Reset W7L1 sketch

--- Lectures/W7L1_03_Procedural_Shapes_Dynamic/sketch.js
+++ Lectures/W7L1_03_Procedural_Shapes_Dynamic/sketch.js
@@ -1,29 +1,15 @@
 function setup() {
-    // const numVertices = 12;
-    // const spacing = 360 / numVertices;
-    // createCanvas(400,400);
-    // translate(width/2, height/2);
-    // beginShape();
-    
-    // for(let i = 0; i < numVertices; i++) {
-    //   const x = cos(radians(i * spacing)) * 100;
-    //   const y = sin(radians(i * spacing)) * 100;
-    //   vertex(x, y);
-    // }
-    // endShape(CLOSE);
+  createCanvas(500, 500);
 }
 
 function draw() { 
-    let numVertices = map(mouseX,0,width*2,0,360);
-    const spacing = 360 / numVertices;
-    createCanvas(400,400);
-    translate(width/2, height/2);
-    beginShape();
-    
-    for(let i = 0; i < numVertices; i++) {
-      const x = cos(radians(i * spacing)) * 100;
-      const y = sin(radians(i * spacing)) * 100;
-      vertex(x, y);
-    }
-    endShape(CLOSE);
+  translate(width/2, height/2); 
+  beginShape(); 
+  for(let i = 0; i < 10; i++) { 
+    const x = cos(radians(i * 36)) * 100;
+    const y = sin(radians(i * 36)) * 100; 
+    vertex(x, y); 
+  }
+  endShape(CLOSE);
+  
 }
